07936abbfa7af7393176aca89023d17d26401614
Vitalya Zvyagin <78ku78@gmail.com>
2020-05-20 01:52:45 +0300

    dz1

--- core/src/com/mygdx/game/Circle.java
+++ core/src/com/mygdx/game/Circle.java
@@ -1,5 +1,39 @@
 package com.mygdx.game;
-class Cicle{
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
+class Circle {
+    private final int RADIUS = 50;
+    private Vector2 fieldSize;
+    private Vector2 pos;
+    Texture texture;
+
+    Circle(int x, int y, Vector2 fieldSize) {
+        this.pos = new Vector2(x, y);
+        this.fieldSize = fieldSize;
+        this.texture = new Texture("circle.png");
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, pos.x - RADIUS, pos.y - RADIUS);
+    }
+
+    public void update(Vector2 goalPos, Vector2 goalSize) {
+
+        float deltaX = Math.abs(pos.x - goalPos.x);
+        float deltaY = Math.abs(pos.y - goalPos.y);
+
+        while (deltaX <= (RADIUS + goalSize.x / 2) && deltaY <= (RADIUS + goalSize.y / 2)) {
+            pos.x = (float) (Math.random() * (fieldSize.x - RADIUS * 2f) + RADIUS);
+            pos.y = (float) (Math.random() * (fieldSize.y - RADIUS * 2f) + RADIUS);
+            deltaX = Math.abs(pos.x - goalPos.x);
+            deltaY = Math.abs(pos.y - goalPos.y);
+        }
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
 
 }
--- core/src/com/mygdx/game/DuneGame.java
+++ core/src/com/mygdx/game/DuneGame.java
@@ -1,4 +1,56 @@
 package com.mygdx.game;
-class DuneGame{
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
+
+public class DuneGame extends ApplicationAdapter {
+
+	private SpriteBatch batch;
+	private SpriteBatch batch2;
+	private Tank tank;
+	private Circle circle;
+	@Override
+	public void create() {
+		Vector2 fieldSize = new Vector2(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+		batch = new SpriteBatch();
+		tank = new Tank(200, 200);
+
+		batch2 = new SpriteBatch();
+		circle = new Circle(700, 400, fieldSize);
+	}
+
+	@Override
+	public void render() {
+		float dt = Gdx.graphics.getDeltaTime();
+		update(dt);
+		Gdx.gl.glClearColor(0, 0.4f, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+		batch.begin();
+		tank.render(batch);
+		circle.render(batch);
+		batch.end();
+	}
+
+	public void update(float dt) {
+		circle.update(tank.getPosition(), tank.getSize());
+		tank.update(dt);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		tank.dispose();
+		circle.dispose();
+	}
+
+	// Домашнее задание:
+	// - Задать координаты точки, и нарисовать в ней круг (любой круг, радиусом пикселей 50)
+	// - Если "танк" подъедет вплотную к кругу, то круг должен переместиться в случайную точку
+	// - * Нельзя давать танку заезжать за экран
 }
