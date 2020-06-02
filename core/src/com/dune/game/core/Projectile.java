b05f44ecf61a77219c424153545c8faba6823bfc
Vitalya Zvyagin <78ku78@gmail.com>
2020-06-03 02:36:17 +0300

    dz5

---
+++ core/src/com/dune/game/core/Projectile.java
@@ -0,0 +1,62 @@
+package com.dune.game.core;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+public class Projectile extends GameObject implements Poolable {
+    private TextureRegion texture;
+    private Vector2 velocity;
+    private float speed;
+    private float angle;
+    private boolean active;
+    private int power;
+    private Tank.Owner ownerType;
+
+    @Override
+    public boolean isActive() {
+        return active;
+    }
+
+    public void deactivate() {
+        active = false;
+    }
+
+    public Projectile(GameController gc) {
+        super(gc);
+        power = 0;
+        this.velocity = new Vector2();
+        this.speed = 640.0f;
+        this.ownerType = Tank.Owner.PLAYER;
+    }
+
+    public void setup(Vector2 startPosition, float angle, TextureRegion texture, int power, Tank.Owner ownerType) {
+        this.texture = texture;
+        this.position.set(startPosition);
+        this.angle = angle;
+        this.velocity.set(speed * MathUtils.cosDeg(angle), speed * MathUtils.sinDeg(angle));
+        this.active = true;
+        this.power = power;
+        this.ownerType = ownerType;
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x - 8, position.y - 8);
+    }
+
+    public void update(float dt) {
+        position.mulAdd(velocity, dt);
+        if (position.x < 0 || position.x > 1280 || position.y < 0 || position.y > 720) {
+            deactivate();
+        }
+    }
+    public int getPower() {
+        return power;
+    }
+
+    public Tank.Owner getOwner() {
+        return ownerType;
+    }
+}
